2afb60352b4b2ea14dd369577c727d1ef320c47c
bobby-riley <bobby.riley.dev@outlook.com>
2021-04-13 10:41:51 -0700

    Try to use octokit

--- .github/workflows/fetch-done-issues.rb
+++ .github/workflows/fetch-done-issues.rb
@@ -1,4 +1,10 @@
 require "octokit"
 
+client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+user = client.user
 
 puts "This is super cool"
+puts "#{user.to_yaml}"
+puts "#{ENV['PROJECT_NWO']}"
+puts "#{ENV['PROJECT_ID']}"
+
