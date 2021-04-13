c1297138793625364d41edab181cea5000cae6b9
bobby-riley <bobby.riley.dev@outlook.com>
2021-04-13 11:12:13 -0700

    who knows3

--- .github/workflows/fetch-done-issues.rb
+++ .github/workflows/fetch-done-issues.rb
@@ -1,28 +1,26 @@
 require "octokit"
+require 'json'
+
+# Some documentation
+# https://octokit.github.io/octokit.rb/Octokit/Client/Projects.html
+# https://docs.github.com/en/rest/reference/projects
 
 nwo = ENV['PROJECT_NWO']
+
 client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
 projects = client.projects(nwo)
 projects.each do |project|
-  # Print the project id: name and the number of open issues
-  puts "#{project.id}: #{project.name} (#{project.open_issues_count})"
-
-  project = client.project(project.id)
+  # TODO: do we want to check that the project name is a match
+  puts "#{project.id}: #{project.name}"
 
   client.project_columns(project.id).each do |column|
     puts "  #{column.id}: #{column.name}"
 
-    client.column_cards(column.id).each do |card|
-      puts "    #{card.id}: #{card.to_json}"
+    if column.name == 'Done'
+      client.column_cards(column.id).each do |card|
+        puts "    #{card.id}: #{card.inspect}"
+        puts card.map(&:to_h).to_json(:indent => 4)
+      end
     end
   end
 end
-
-
-
-
-# puts "This is super cool"
-# puts "#{user.to_yaml}"
-# puts "#{ENV['PROJECT_NWO']}"
-# puts "#{ENV['PROJECT_ID']}"
-
